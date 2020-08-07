af58140c21efa48b184260037c1849617f09218c
adrianocruzweb <adrianocruzweb@gmail.com>
2020-08-07 13:14:58 -0400

    Primeiro Commit do Proffy NLW RocketSeat

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/17552862?s=460&u=85acddc96061558ad5ffa9e9953f53866243de5a&v=4"
+          alt="Adriano Cruz"
+        />
+        <div>
+          <strong>Adriano Cruz</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button>
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em Contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponiveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="week-day" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="time">Matéria</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  );
+}
+
+export default TeacherList;
