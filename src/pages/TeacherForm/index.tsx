b7d2adf543c7720679848db07cec50890f390459
adrianocruzweb <adrianocruzweb@gmail.com>
2020-08-12 09:38:13 -0400

    Commit apó uma hora da 3ª aula (3º dia) ---(Erro estili "+novo horário")

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,77 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/input';
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 function TeacherForm() {
   return (
-    <div id="page-teacher-list" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas" />
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
+        </fieldset>
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
+          <Input name="cost" label="Custo da sua Hora por aula" />
+        </fieldset>
+        <fieldset>
+          <legend>
+            Horário Disponíveis
+            <button type="button">+ Novo Horário</button>
+          </legend>
+          <div className="schedule-item">
+            <Select
+              name="week-day"
+              label="Dia da semana"
+              options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda-feira' },
+                { value: '2', label: 'Terça-feira' },
+                { value: '3', label: 'Quarta-feira' },
+                { value: '4', label: 'Quinta-feira' },
+                { value: '5', label: 'Sexta-feira' },
+                { value: '6', label: 'Sábado' },
+              ]}
+            />
+            <Input name="From" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante" />
+            Importante!
+            <br />
+            Preencha Todos os Dados
+          </p>
+          <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
